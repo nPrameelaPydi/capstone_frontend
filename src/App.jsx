7707100ec8e45eba16c63e533c22774624495a5b
Prameela Pydi <nprameela.pydi@gmail.com>
2024-11-21 11:58:00 -0800

    add show password checkbox

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Nav from "../components/Nav.jsx";
 import RecipesPage from "../pages/RecipesPage.jsx";
 import ChatsPage from "../pages/ChatsPage";
 import Register from "../pages/RegistrationPage.jsx";
+import Login from "../pages/LoginPage.jsx";
 
 export default function App() {
   return (
@@ -14,6 +15,7 @@ export default function App() {
         <Route path="/" element={<HomePage />}></Route>
         <Route path="/recipes" element={<RecipesPage />}></Route>
         <Route path="/register" element={<Register />}></Route>
+        <Route path="/login" element={<Login />}></Route>
 
         <Route path="/chats" element={<ChatsPage />}></Route>
         <Route path="/*" element={<NotFound />}></Route>
