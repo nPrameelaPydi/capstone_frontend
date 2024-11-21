8dd737fd18154b1edc61eb7f035c65ad7e99f6e0
Prameela Pydi <nprameela.pydi@gmail.com>
2024-11-21 11:56:36 -0800

    add forgot password feature

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,7 +6,7 @@ export default function Nav() {
       <Link to="/">Home</Link>
       <Link to="/recipes">Recipes</Link>
       <Link to="/register">Register</Link>
-      {/*<Link to="/login">Login</Link>*/}
+      <Link to="/login">Login</Link>
       <Link to="/chats">Chats</Link>
     </nav>
   );
--- pages/RegistrationPage.jsx
+++ pages/RegistrationPage.jsx
@@ -10,6 +10,8 @@ const Register = () => {
   });
   // To show success/error message after submitting the form
   const [message, setMessage] = useState("");
+  // to show password feature
+  const [showPassword, setShowPassword] = useState(false);
 
   const handleChange = (e) => {
     // update form data object with changed value
@@ -24,11 +26,13 @@ const Register = () => {
         "http://localhost:3000/api/auth/register",
         formData
       );
-      console.log(response.data); // Log success response
+      console.log(response);
+      console.log(`${response.config.data}`);
+      console.log(response.data.message);
       setMessage(response.data.message); //set success msg
     } catch (error) {
       console.error(error.response?.data || error.message);
-      //error.response?.data?.message: The ?. is called optional chaining. It ensures that if response or data is null or undefined, it won’t throw an error and will return undefined instead. This makes it safer to access properties that might not exist if something goes wrong with the request.
+      // optional chaining ensures that if response or data is null or undefined, it won’t throw an error and will return undefined instead. This makes it safer to access properties that might not exist if something goes wrong with the request.
       setMessage(error.response?.data?.message || "Registration failed");
     }
   };
@@ -60,12 +64,21 @@ const Register = () => {
         <div>
           <label htmlFor="">Password</label>
           <input
-            type="password"
+            type={showPassword ? "text" : "password"}
             name="password"
             value={formData.password}
             onChange={handleChange}
             required
           ></input>
+          <label htmlFor="">
+            {/* toggle showPassword state */}
+            <input
+              type="checkbox"
+              checked={showPassword}
+              onChange={() => setShowPassword(!showPassword)}
+            />
+            Show Password
+          </label>
         </div>
         <button type="submit">Register</button>
       </form>
