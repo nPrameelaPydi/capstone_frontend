cfc4eb0b2a287bacf66c6045e3f7ef04b615fc4f
Prameela Pydi <nprameela.pydi@gmail.com>
2024-11-21 23:30:15 -0800

    modified createdBy field in the recipe form to handle and display usernames instead of ObjectId.

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -4,11 +4,12 @@ export default function Recipe({ recipe, onDelete, onEdit }) {
       <h2>{recipe.title}</h2>
       <p>{recipe.ingredients.join(", ")}</p>
       <p>{recipe.instructions}</p>
-      <p>Created by: {recipe.createdBy}</p>
+      <p>Created by: {recipe.createdBy.name || "Unknown"}</p>{" "}
+      {/* Display the user's name */}
       <p>Created at: {new Date(recipe.createdAt).toLocaleString()}</p>
       <button onClick={() => onEdit()}>Edit</button> {/* Edit button */}
+      <button onClick={() => onDelete(recipe._id)}>Delete Recipe</button>{" "}
       {/* Delete button */}
-      <button onClick={() => onDelete(recipe._id)}>Delete Recipe</button>
     </div>
   );
 }
