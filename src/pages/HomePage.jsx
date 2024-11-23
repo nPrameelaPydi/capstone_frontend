daaa6afcd8f347fffb71f464215572b16cd79a46
Prameela Pydi <nprameela.pydi@gmail.com>
2024-11-23 14:58:52 -0800

    display all recipes on HomePage on initial load

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import SearchBar from "../components/SearchBar.jsx";
 import RecipeList from "../components/RecipeList.jsx";
+import axios from "axios";
 
 export default function HomePage() {
   //state to hold list of recipes
   const [recipes, setRecipes] = useState([]);
+  //state to handle errors
+  const [error, setError] = useState("");
+
+  //Fetch all recipes on initialload of Homepage
+  useEffect(() => {
+    const fetchAllRecipes = async () => {
+      try {
+        const response = await axios.get("http://localhost:3000/api/recipes"); //axios GET request
+        setRecipes(response.data); //update recipes state with response data
+      } catch (error) {
+        console.error("Error fetching recipes", error);
+        setError("Failed to fetch recipes");
+      }
+    };
+    fetchAllRecipes();
+  }, []);
 
   //update the recipes state when search results are received
   const handleSearchResults = (recipeList) => {
@@ -16,6 +33,8 @@ export default function HomePage() {
       <h1>Welcome to Food & Friends</h1>
       <SearchBar onSearch={handleSearchResults}></SearchBar>
       <RecipeList recipes={recipes}></RecipeList>
+      {/* display error msgs if any */}
+      {error && <p>{error}</p>}
     </div>
   );
 }
