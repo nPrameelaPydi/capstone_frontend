5c00077ccceea10e2629d126ceba50900f32cdf9
Prameela Pydi <nprameela.pydi@gmail.com>
2024-11-23 21:40:11 -0800

    recipe card display css

--- src/components/RecipeList.jsx
+++ src/components/RecipeList.jsx
@@ -7,16 +7,18 @@ export default function RecipeList({ recipes, onRecipeClick }) {
       {recipes.length === 0 ? (
         <p>No recipes found</p>
       ) : (
-        <ul>
-          {recipes.map((recipe) => (
-            <li key={recipe._id} onClick={() => onRecipeClick(recipe._id)}>
-              <h3>{recipe.title}</h3>
-              {recipe.image && <img src={recipe.image} alt={recipe.title} />}
-              <p>{recipe.ingredients.join(", ")}</p>
-              <p>{recipe.instructions}</p>
-            </li>
-          ))}
-        </ul>
+        <div className="recipe-list">
+          <ul>
+            {recipes.map((recipe) => (
+              <li key={recipe._id} onClick={() => onRecipeClick(recipe._id)}>
+                <h3>{recipe.title}</h3>
+                {recipe.image && <img src={recipe.image} alt={recipe.title} />}
+                <p>{recipe.ingredients.join(", ")}</p>
+                <p>{recipe.instructions}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
       )}
     </div>
   );
